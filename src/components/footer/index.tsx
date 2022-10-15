257ab91350cda5b44b18658f80257e7c1792c42d
Sergey Kovalchuk <kovalchuk.s.v@inbox.ru>
2022-10-16 00:59:59 +0700

    feat: create left footer block

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,55 @@
+import s from './styles.module.scss'
+import {footerTextRight, iconsFooter} from '../../mocks/footer'
+
 export const Footer = () => {
   return (
-    <div>
-      <div>
-        Footer left
+    <div className={s.wrapper}>
+      <div className={s.left}>
+        <p className={s.left__undertitle}>
+          Социальный проект
+        </p>
+        <p className={s.left__title}>
+          HelpPet
+        </p>
+        <p className={s.left__subtitle}>
+          (Тут о надо написать о проекте, кто его создал и почему). Для добрых дел время всегда найдётся. Это доказал
+          человек, ездивший в Питер по своим делам. Как-то проходя один из дворов, он обнаружил там хорошую молоденькую
+          кошечку, совсем ещё подростка. Он приманил её и забрал с собой в столицу. В дороге человек и кошка почти
+          подружились и узнали друг друга получше.
+        </p>
+        <div className={s.left__icons}>
+          {
+            iconsFooter.map(icon => (
+              <div
+                key={icon.id}
+                className={s.left__social}
+              >
+                <img
+                  className={s.left__icon}
+                  src={icon.src}
+                />
+              </div>
+            ))
+          }
+        </div>
       </div>
-      <div>
-        Footer right
+      <div className={s.right}>
+        {
+          footerTextRight.map(item => (
+            <>
+              <h2 key={item.id}>
+                {item.title}
+              </h2>
+              {
+                item.subtitleLinks.map(link => (
+                  <a key={link.id}>
+                    {link.text}
+                  </a>
+                ))
+              }
+            </>
+          ))
+        }
       </div>
     </div>
   )
---
+++ src/mocks/footer/index.ts
@@ -0,0 +1,108 @@
+import Instagram from '../../assets/icons/Instagram.svg'
+import Youtube from '../../assets/icons/Youtube.svg'
+import Twitter from '../../assets/icons/Twitter.svg'
+
+import Mail from '../../assets/icons/Mail.svg'
+import Telegram from '../../assets/icons/Telegram.svg'
+import Facebook from '../../assets/icons/Facebook.svg'
+import Vk from '../../assets/icons/VK.svg'
+
+
+export const iconsFooter = [
+  {
+    id: 1,
+    src: Instagram,
+    alt: 'instagram icon'
+  },
+  {
+    id: 2,
+    src: Youtube,
+    alt: 'youtube icon'
+  },
+  {
+    id: 3,
+    src: Twitter,
+    alt: 'twitter icon'
+  }
+]
+
+export const footerTextRight = [
+  {
+    id: 1,
+    title: 'Главная',
+    subtitleLinks: [
+      {
+        id: 1,
+        text: 'Помочь нам'
+      },
+      {
+        id: 2,
+        text: 'Питомцы'
+      },
+      {
+        id: 3,
+        text: 'История спасения'
+      },
+      {
+        id: 4,
+        text: 'О проекте'
+      },
+      {
+        id: 5,
+        text: 'Отчёты'
+      },
+      {
+        id: 6,
+        text: 'Реквезиты'
+      },
+      {
+        id: 7,
+        text: 'Вакансии'
+      },
+      {
+        id: 8,
+        text: 'Мы в СМИ'
+      }
+    ]
+  }
+  // {
+  //   id: 2,
+  //   title: 'Контакты',
+  //   subtitleLinks: [
+  //     {
+  //       id: 1,
+  //       src: Mail
+  //     },
+  //     {
+  //       id: 2,
+  //       src: Telegram
+  //     },
+  //     {
+  //       id: 3,
+  //       src: Facebook
+  //     },
+  //     {
+  //       id: 4,
+  //       src: Vk
+  //     }
+  //   ]
+  // },
+  // {
+  //   id: 3,
+  //   title: 'Сотрудничество',
+  //   subtitleLinks: [
+  //     {
+  //       id: 1,
+  //       text: 'Волонтерам'
+  //     },
+  //     {
+  //       id: 2,
+  //       text: 'Ветклиникам'
+  //     },
+  //     {
+  //       id: 3,
+  //       text: 'Приютам'
+  //     }
+  //   ]
+  // }
+]
