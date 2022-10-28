fba6015b60f656bb71f877070345034f85f32dce
Sergey Kovalchuk <kovalchuk.s.v@inbox.ru>
2022-10-29 00:14:59 +0700

    feat: add widget for sections and gilroy

---
+++ src/components/widgets/index.ts
@@ -0,0 +1,1 @@
+export { default as Cooperation } from './Cooperation'
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,10 @@
-import { useTranslation } from 'react-i18next'
-
-import { BaseButton } from '../../components/index'
-import cooperation from '../../assets/icons/home/cooperation-image.svg'
-
-import s from './styles.module.scss'
+import { Cooperation } from '../../components/widgets'
 
 export const Home = () => {
 
-  const { t } = useTranslation()
-
-  const reset = () => {
-    console.log(1)
-  }
   return (
-    <div className={s.cooperation}>
-      <img className={s.cooperation__img} src={cooperation} alt="Сотрудничество"/>
-      <p className={s.cooperation__title}>{t('cooperation.title')}</p>
-      <p className={s.cooperation__subtitle}>{t('cooperation.subtitle')}</p>
-      <div className={s.cooperation__button}>
-        <BaseButton
-          variant='filled'
-          color='primary'
-          click={reset}>
-          Начать сотрудничать
-        </BaseButton>
-      </div>
+    <div>
+      <Cooperation/>
     </div>
   )
 }
