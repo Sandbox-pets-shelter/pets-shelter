6a03a119fd255e78a30584e538c912d27dd1dd4f
Sergey Kovalchuk <kovalchuk.s.v@inbox.ru>
2022-10-28 12:39:09 +0700

    fix error

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,25 @@
-import { BaseButton } from '../../components/ui/BaseButton'
+import { BaseButton } from '../../components/index'
 import s from './styles.module.scss'
 import people from '../../assets/icons/home/people-cat.png'
 
 export const Home = () => {
-    const reset = () => {
-       console.log(1)
-      }
-    return (
-        <div className={s.home}>
-            <img className={s.home__img} src={people} alt="Люди и кот"/>
-            <p className={s.home__item1}>Сотрудничая с нами вы спасаете Жизни!</p>
-            <p className={s.home__item2}>Если вы являетесь неравнодушным человеком, у которого<br></br>
-            есть предложения по сотрудничеству с приютами, напишите нам!</p>
-            <button className={s.home__button}>
-                <BaseButton 
-                    variant='filled' 
-                    color='primary'
-                    click={reset}>
-                    Начать сотрудничать
-                </BaseButton>
-            </button>    
-      </div>
-    )
+  const reset = () => {
+    console.log(1)
+  }
+  return (
+    <div className={s.home}>
+      <img className={s.home__img} src={people} alt="Люди и кот"/>
+      <p className={s.home__item1}>Сотрудничая с нами вы спасаете Жизни!</p>
+      <p className={s.home__item2}>Если вы являетесь неравнодушным человеком, у которого<br></br>
+        есть предложения по сотрудничеству с приютами, напишите нам!</p>
+      <button className={s.home__button}>
+        <BaseButton
+          variant='filled'
+          color='primary'
+          click={reset}>
+          Начать сотрудничать
+        </BaseButton>
+      </button>
+    </div>
+  )
 }
