9fc824b11c8dbee01357089f5c62bcb42ee060bd
Tinanevich <Kristina@qmail.com>
2022-11-23 11:34:56 +0300

    add-corrections

--- src/components/widgets/NeedHelp/index.tsx
+++ src/components/widgets/NeedHelp/index.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next'
-import { iconsNeedHelp } from '../../../mocks/needhelp'
+import { itemsNeedHelp } from '../../../mocks/needhelp'
 import arrow from '../../../assets/icons/needhelp/arrow.svg'
 
 import s from './styles.module.scss'
@@ -10,26 +10,26 @@ const NeedHelp = () => {
 
   return (
     <div className={s.needhelp}>
-      <p className={s.needhelp__title}>{t('needhelp.title')}</p>
+      <p className={s.needhelp__title}>{t('needhelp.main_title')}</p>
       <div className={s.needhelp__container}>
         {
-          iconsNeedHelp.map(icon => (
+          itemsNeedHelp.map(item => (
             <div
-              key={icon.id}
+              key={item.id}
               className={s.needhelp__container__item}
             >
-                <figure className={s.needhelp__container__img}> 
+                <div className={s.needhelp__container__img}> 
                     <img
-                        src={icon.src}
-                        alt={icon.alt}
+                        src={item.src}
+                        alt={item.alt}
                     />
-                    <figcaption className={s.needhelp__container__img__title}>
-                        <p className={s.needhelp__container__img__subtitle}>{icon.subtitle}</p>
-                        <p className={s.needhelp__container__img__content}>{icon.content}</p>
-                        <a className={s.needhelp__container__img__title__btm}href="#">{icon.link}
+                    <div className={s.needhelp__container__img__title}>
+                        <p className={s.needhelp__container__img__subtitle}>{item.subtitle}</p>
+                        <p className={s.needhelp__container__img__content}>{item.content}</p>
+                        <a className={s.needhelp__container__img__title__btm}href="#">{item.link}
                         <img className={s.needhelp__container__img__title__btm__arr}src={arrow} alt="Стрелка"/></a>
-                        </figcaption>
-                </figure> 
+                        </div>
+                </div> 
          </div>
           ))
         }
--- src/mocks/needhelp/index.ts
+++ src/mocks/needhelp/index.ts
@@ -4,29 +4,29 @@ import Feed from '../../assets/icons/needhelp/feed.svg'
 
 import i18n from '../../i18n'
 
-export const iconsNeedHelp = [
+export const itemsNeedHelp = [
     {
       id: 1,
       src: Volunteer,
-      alt: i18n.t('Volunteer'),
-      subtitle: 'Стать волонтёром',
-      content: 'Вы можете покормить, погулять или поиграть с животными в приюте.',
-      link: 'Узнать подробнее'
+      alt: i18n.t('needhelp.volunteer'),
+      subtitle: i18n.t('needhelp.volunteer.subtitle'),
+      content: i18n.t('needhelp.volunteer.content'),
+      link: i18n.t('needhelp.volunteer.btn_text')
     },
     {
       id: 2,
       src: Carring,
-      alt: i18n.t('Carring'),
-      subtitle: 'Приютить на время',
-      content: 'В приютах не хватает мест и мы нуждаемся в людях!',
-      link: 'Заполнить анкету'
+      alt: i18n.t('needhelp.carring'),
+      subtitle: i18n.t('needhelp.carring.subtitle'),
+      content: i18n.t('needhelp.carring.content'),
+      link: i18n.t('needhelp.carring.btn_text')
     },
     {
       id: 3,
       src: Feed,
-      alt: i18n.t('Feed'),
-      subtitle: 'Подарить радость',
-      content: 'Нашим подопечным будет приятно любое внимание!',
-      link: 'Купить подарок'
+      alt: i18n.t('needhelp.feed'),
+      subtitle: i18n.t('needhelp.feed.subtitle'),
+      content: i18n.t('needhelp.feed.content'),
+      link: i18n.t('needhelp.feed.btn_text')
     }
   ]
