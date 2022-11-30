adf1ce19151d4f044e84b1189df23b82d48eef67
Tinanevich <Kristina@qmail.com>
2022-12-01 01:07:02 +0300

    add feature block main first and urgent help

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,13 @@ import { Sponsors } from '../../components/widgets'
 import { NeedHelp } from '../../components/widgets'
 import { HomelessPet } from '../../components/widgets'
 import { HappyStories } from '../../components/widgets'
+import { MainFirst } from '../../components/widgets'
 
 export const Home = () => {
 
   return (
     <div>
+      <MainFirst/>
       <HomelessPet/>
       <HappyStories/>
       <NeedHelp/>
