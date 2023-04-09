b394243dc614bdebd89a01c96981cf9216a600c3
Venger Yuriy <venger.yuriy@icloud.com>
2023-04-09 23:59:52 +0200

    clear about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,15 +1,3 @@
-import Textarea from 'components/ui/Textarea';
-import EmojiPicker from 'emoji-picker-react';
-import { ChangeEvent } from 'react';
-
 export const About = () => {
-  const printTextarea = (e: any) => console.log(e);
-  const printInput = (e: ChangeEvent<HTMLInputElement>) => console.log(e);
-
-  return (
-    <div>
-      About
-      <Textarea onFileUpload={printInput} onChangeTextarea={printTextarea} />
-    </div>
-  );
+  return <div>About</div>;
 };
