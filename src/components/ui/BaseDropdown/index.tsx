972bd2fba8d8a91893052c2c833fa16f962a70a5
Beksultan Sagnaev <36114003+beksultanweb@users.noreply.github.com>
2023-05-28 20:17:00 +0600

    Update index.tsx

--- src/components/ui/BaseDropdown/index.tsx
+++ src/components/ui/BaseDropdown/index.tsx
@@ -14,9 +14,21 @@ const BaseDropdown: FC<BaseDropdownProps> = ({ items }) => {
   const title = items[0];
   const dropdownItems = items.slice(1);
 
+  const clickOutside = () => {
+    setOpen(false);
+    document.removeEventListener('click', clickOutside);
+  };
+
+  const toggleDropdown = async () => {
+    setOpen((prev) => !prev);
+    setTimeout(() =>
+      isOpen ? document.removeEventListener('click', clickOutside) : document.addEventListener('click', clickOutside)
+    );
+  };
+
   return (
     <div className={s.dropdown} onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
-      <span>
+      <span onClick={toggleDropdown}>
         {title.name}
         <img src={ArrowDown} style={{ transform: isOpen ? 'rotateX(180deg)' : 'none' }} />
       </span>
