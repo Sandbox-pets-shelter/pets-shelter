5d6863dbeaae2d88539ed1e289bc3f19f20c1f25
Venger Yuriy <venger.yuriy@icloud.com>
2023-07-27 15:04:24 +0200

    delete unnessesary fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css';
-import { getRequest } from 'api';
 import PopupsContainer from 'components/popups/PopupsContainer';
-import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { routeElements } from './configs/routeElements';
 
 function App() {
-  useEffect(() => {
-    getRequest('/pet');
-  }, []);
-
   return (
     <div className="container">
       <Routes>
